e77afe15eb832fb039ed13d19738a5e349fc8718
tpgurdl92 <tpgurdl92@gmail.com>
2020-02-15 12:12:38 +0900

    Updates

---
+++ precache-manifest.63b093d25d9a718a70f28b8b24ba2545.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20ccfe81bcdc4d6451b6c258d997a0b1",
+    "url": "/tpgurdl92/movielist.git/index.html"
+  },
+  {
+    "revision": "722b0d311956fd762f42",
+    "url": "/tpgurdl92/movielist.git/static/css/main.413d4422.chunk.css"
+  },
+  {
+    "revision": "cdf6ce39e2f8de65710b",
+    "url": "/tpgurdl92/movielist.git/static/js/2.2b2117cd.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/tpgurdl92/movielist.git/static/js/2.2b2117cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "722b0d311956fd762f42",
+    "url": "/tpgurdl92/movielist.git/static/js/main.2d426170.chunk.js"
+  },
+  {
+    "revision": "2aaa504921e9d20550da",
+    "url": "/tpgurdl92/movielist.git/static/js/runtime-main.bb032826.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/tpgurdl92/movielist.git/static/media/logo.5d5d9eef.svg"
+  }
+]);
